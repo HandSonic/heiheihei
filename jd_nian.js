6036711941414346af091d77a76cea4ad15fc842
HandSonic <8078023+HandSonic@users.noreply.github.com>
2021-01-21 16:18:45 +0800

    Update jd_nian.js

--- jd_nian.js
+++ jd_nian.js
@@ -27,8 +27,8 @@ const $ = new Env('京东炸年兽🧨');
 const notify = $.isNode() ? require('./sendNotify') : '';
     console.log('28\n');
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
+const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';    console.log('?');
+let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
     console.log('32\n');
 const randomCount = $.isNode() ? 20 : 5;
 //IOS等用户直接用NobyDa的jd cookie
